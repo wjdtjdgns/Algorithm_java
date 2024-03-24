88531b7ef478ef8b44e33637a9f974132a726203
wjdtjdgns <person3275@gmail.com>
2024-03-24 21:19:54 +0900

    Baekjoon 15688 - 수 정렬하기 5

---
+++ src/BOJ15688.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class BOJ15688 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            arr[i] = num;
+        }
+        Arrays.sort(arr);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : arr) {
+            sb.append(i).append("\n");
+        }
+        System.out.println(sb.toString());
+    }
+}
